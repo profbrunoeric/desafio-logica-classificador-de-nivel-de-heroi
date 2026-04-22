1d9c8559aa5c580a8b162e14dcfabf9e27b0d96d
profbrunoeric <prof.brunoeric@gmail.com>
2026-04-22 08:54:36 -0300

    atualização com if else adicionados

--- index.js
+++ index.js
@@ -19,6 +19,26 @@
 // "O nome do herói é [NOME] e ele é do nível [NÍVEL]"
 
 let nome = "John";
+let XP = "";
 
+if (XP <= 1000) {
+    let nivel = "ferro";
+} else if (XP >= 1001 && XP <= 2000) {
+    let nivel = "bronze";
+} else if (XP >= 2001 && XP <= 5000) {
+    let nivel = "prata";
+} else if (XP >= 5001 && XP <= 7000) {
+    let nivel = "ouro";
+} else if (XP >= 7001 && XP <= 8000) {
+    let nivel = "platina";
+} else if (XP >= 8001 && XP <= 9000) {
+    let nivel = "ascendente";
+} else if (XP >= 9001 && XP <= 10000) {
+    let nivel = "imortal";
+} else if (XP >= 10001) {
+    let nivel = "radiante";
+}
 
-console.log("O nome do herói é " + nome);
+for (nivel
+
+console.log("O nome do herói é " + nome + " e ele é do nível " + nivel + ".");
